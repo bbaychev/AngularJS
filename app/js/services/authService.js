a27672ef25111f380ae9ab5a84705cc06e78abae
bbaychev <mb_int@abv.com>
2015-05-23 11:53:54 +0300

    Added Register/Login page and controller
    They're still not operational though, but they exist!

---
+++ app/js/services/authService.js
@@ -0,0 +1,105 @@
+'use strict';
+
+app.factory('authService',
+    function ($http, baseServiceUrl) {
+        return {
+            login: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/Login',
+                    data: userData
+                };
+                $http(request).success(function(data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            register: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/Register',
+                    data: userData
+                };
+                $http(request).success(function(data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            logout: function(success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/Logout'
+                };
+                //TODO: Check if this whole things even works
+                $http(request).success(function(success) {
+                    delete sessionStorage['currentUser'];
+                }).error(error);
+            },
+
+            getCurrentUser : function() {
+                var userObject = sessionStorage['currentUser'];
+                if (userObject) {
+                    return JSON.parse(sessionStorage['currentUser']);
+                }
+            },
+
+            isAnonymous : function() {
+                return sessionStorage['currentUser'] == undefined;
+            },
+
+            isLoggedIn : function() {
+                return sessionStorage['currentUser'] != undefined;
+            },
+
+            isNormalUser : function() {
+                var currentUser = this.getCurrentUser();
+                return (currentUser != undefined) && (!currentUser.isAdmin);
+            },
+
+            isAdmin : function() {
+                var currentUser = this.getCurrentUser();
+                return (currentUser != undefined) && (currentUser.isAdmin);
+            },
+
+            getAuthHeaders : function() {
+                var headers = {};
+                var currentUser = this.getCurrentUser();
+                if (currentUser) {
+                    headers['Authorization'] = 'Bearer ' + currentUser.access_token;
+                }
+                return headers;
+            },
+
+            getUserProfile: function(username, success, error) {
+                var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/users/' + username + '/preview',
+                    headers: this.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            editProfile: function(userData, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/me',
+                    data: userData,
+                    headers: this.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            changePass: function(passData, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/me/ChangePassword',
+                    data: passData,
+                    headers: this.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            }
+        }
+    }
+);
